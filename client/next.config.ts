e9598032f3ef55bbfeee82646ce64026f4bc0e3f
Nuriddin <abdulxayevnuriddin01@gmail.com>
2025-06-21 16:09:28 +0500

    .

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,12 +1,14 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	images: {
-		remotePatterns: [
-			{ protocol: 'https', hostname: 'github.com', pathname: '**' },
-			{ protocol: 'https', hostname: 'utfs.io', pathname: '**' },
-		],
-	},
-}
+  output: "standalone",
 
-export default nextConfig
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "github.com", pathname: "**" },
+      { protocol: "https", hostname: "utfs.io", pathname: "**" },
+    ],
+  },
+};
+
+export default nextConfig;
